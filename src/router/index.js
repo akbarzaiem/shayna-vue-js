86757fa1537ce375256b5a804a022d47068a2bda
akbarzaiem <akbarzaiemp@gmail.com>
2021-09-18 22:23:58 +0700

    first upload

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Product.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ShoppingCart.vue')
+  },
+  {
+    path: '/succes',
+    name: 'Succes',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Succes.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomanBanner />
+    <Instagram />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import WomanBanner from "@/components/WomanBanner.vue";
+import Instagram from "@/components/Instagram.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    Header,
+    Hero,
+    WomanBanner,
+    Instagram,
+    PartnerLogo,
+    Footer,
+  },
+};
+</script>
+
+
